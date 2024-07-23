8bef7dfc265bdbc9c4961394e68d4d06f8d6befe
Danil Semenchin <semen4in.d@gmail.com>
2024-07-23 22:58:00 +0300

    First commit

---
+++ src/main/java/ru/semyak/book/models/Coffee.java
@@ -0,0 +1,42 @@
+package ru.semyak.book.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
+public class Coffee {
+
+    @Id
+    private String id;
+    private String name;
+
+    public Coffee() {}
+
+    public Coffee(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Coffee(String name) {
+        this(UUID.randomUUID().toString(), name);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
